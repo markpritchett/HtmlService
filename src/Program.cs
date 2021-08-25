375a6052cdf99b6b480e6850e520de06ebdb6746
Mark Pritchett <markpritchett@hotmail.co.uk>
2021-08-25 19:51:57 +0100

    refactor: routes
    move routes to own file to separate these from infrastructure code

--- src/Program.cs
+++ src/Program.cs
@@ -1,24 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddHtmlGeneration();
 builder.Services.AddSwaggerWithVersions();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (builder.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-}
-
-app.UseSwaggerWithVersions();
 app.UseHttpsRedirection();
-
-app.MapPost("v1/welcome-email", async (WelcomeEmailModelV1 model, Html html) =>
-            await html.Content("WelcomeEmailV1", model));
-
-app.MapPost("v2/welcome-email", async (WelcomeEmailModelV2 model, Html html) =>
-    await html.Content("WelcomeEmailV2", model));
-
+app.MapApiRoutes();
+app.UseSwaggerWithVersions();
 app.Run();
---
+++ src/Routes.cs
@@ -0,0 +1,13 @@
+﻿namespace HtmlService;
+
+public static class Routes
+{
+    public static void MapApiRoutes(this WebApplication app)
+    {
+        app.MapPost("v1/welcome-email", async (WelcomeEmailModelV1 model, Html html) =>
+            await html.Content("WelcomeEmailV1", model));
+
+        app.MapPost("v2/welcome-email", async (WelcomeEmailModelV2 model, Html html) =>
+            await html.Content("WelcomeEmailV2", model));
+    }
+}
